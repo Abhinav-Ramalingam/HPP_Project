05650023fb7f2f68d408ad92e92e6ce01605ca06
Abhinav-Ramalingam <abhinav.ramalingam.2263@student.uu.se>
2025-03-21 20:41:03 +0100

    Update: Function Prototype

--- qsp3.c
+++ qsp3.c
@@ -4,31 +4,6 @@
 #include <string.h>
 #include <sys/time.h>
 
-// Standard quicksort function
-void local_sort(int *arr, int begin, int end) {
-    if (begin >= end) return;
-
-    int pivot = arr[end]; 
-    int i = begin - 1;
-
-    for (int j = begin; j < end; j++) {
-        if (arr[j] <= pivot) {
-            i++;
-            int temp = arr[i];
-            arr[i] = arr[j];
-            arr[j] = temp;
-        }
-    }
-
-    int temp = arr[i + 1];
-    arr[i + 1] = arr[end];
-    arr[end] = temp;
-
-    local_sort(arr, begin, i);
-    local_sort(arr, i + 2, end);
-}
-
-
 typedef struct static_args_t {
     int   N;
     int NT;
@@ -43,7 +18,130 @@ typedef struct args_t {
     static_args_t* static_args;
 } args_t;
 
-static void* global_sort(void* targs) {
+void* global_sort(void *);
+void local_sort(int *, int, int);
+
+int main(int ac, char** av) {
+    if (ac != 6) {
+        printf("Usage: ./%s N input output NT S\n", av[0]);
+        printf("N - Number of Elements to Sort\n");
+        printf("input - Input Numbers Dataset Filepath (generate with gen_data.c)\n");
+        printf("output - Filepath to store the sorted elements\n");
+        printf("NT - Number of Processors to Utilise\n");
+        printf("strat - Strategy to Select the pivot element:\n");
+        printf("\t\'a\' - Pick Median of Processor-0 in Processor Group\n");
+        printf("\t\'b\' - Select the mean of all medians in respective processor set\n");
+        printf("\t\'c\' - Sort the medians and select the mean value of the two middlemost medians in each processor set\n");
+
+        return 1;
+    }
+    int N = atoi(av[1]);
+    char *inputfile = av[2];
+    char *outputfile = av[3];
+    int NT = atoi(av[4]);
+    char strat = av[5][0];
+
+    /**** PHASE 1: READ INPUT FROM FILE ****/
+
+    int *arr = (int *)calloc(N, sizeof(int));
+    if (arr == NULL) {
+        perror("Memory allocation failed");
+        return 1;
+    }
+
+    FILE *input_fp = fopen(inputfile, "r");
+    if (input_fp == NULL) {
+        perror("Failed to open input file");
+        free(arr);
+        return 1;
+    }
+
+    // Reading integers from the input file
+    for (int i = 0; i < N; i++) {
+        if (fscanf(input_fp, "%d", &arr[i]) != 1) {
+            printf("Error reading number at index %d\n", i);
+            break;
+        }
+    }
+    fclose(input_fp);
+
+
+    /**** PHASE 2: SORT INPUT ****/
+    int** thread_local_arr = (int**) malloc(NT * sizeof(int*)); // local subarrays
+    int** exchange_arr = (int**) malloc(NT * sizeof(int*)); // remote subarrays
+    int*  exchange_arr_sizes = (int*)  malloc(NT * sizeof(int )); // number of elements 'sent'
+    int*  pivots  = (int*)  malloc(NT * sizeof(int )); // pivot element for each thread
+    int*  local_sizes  = (int*)  malloc(NT * sizeof(int )); // number of elements in each thread in arr
+    int*  medians  = (int*)  malloc(NT * sizeof(int )); // median of each subarray
+    
+    pthread_barrier_t* bar_pair = (pthread_barrier_t*) malloc(NT * sizeof(pthread_barrier_t));
+    for (int i = 0; i < NT; i++)
+        pthread_barrier_init(bar_pair + i, NULL, 2);
+    
+    // let T = 16, then
+    // bar_group = [ 0, 0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 6, 7]
+    // b_counts = [16, 8, 8, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2]
+    // num_bs_g =  1  +  2  +     4     +          8
+    int bar_group_count = 0;
+    for (int j = 1; j < NT; j = j << 1)
+        bar_group_count += j;  
+    pthread_barrier_t* bar_group = (pthread_barrier_t*) malloc(bar_group_count * sizeof(pthread_barrier_t));
+    int l = 0;
+    for (int j = 1; j < NT; j = j << 1)
+        for (int k = 0; k < j; k++)
+            pthread_barrier_init(bar_group + l++, NULL, NT / j);
+    
+    static_args_t static_args = {N, NT, strat, thread_local_arr, exchange_arr, arr, exchange_arr_sizes, pivots, local_sizes, medians, bar_pair, bar_group};
+    pthread_t* threads = (pthread_t*) malloc(NT * sizeof(pthread_t));
+    
+    // fork
+    for (int i = 0; i < NT; i++) {
+        // malloc because otherwise it will reuse pointers or something
+        args_t* targs = (args_t*) malloc(sizeof(args_t));
+        targs->threadid = i;
+        targs->static_args = &static_args;
+        pthread_create(threads + i, NULL, global_sort, (void*) targs);
+    }
+    
+    // join
+    for (int i = 0; i < NT; i++)
+        pthread_join(threads[i], NULL);
+    
+    // free shared memory
+    free(thread_local_arr);
+    free(exchange_arr);
+    free(exchange_arr_sizes);
+    free(pivots);
+    free(local_sizes);
+    free(medians);
+    free(threads);
+    for (int i = 0; i < NT; i++)
+        pthread_barrier_destroy(bar_pair + i);
+    for (int i = 0; i < bar_group_count; i++)
+        pthread_barrier_destroy(bar_group + i);
+    free(bar_pair);
+    free(bar_group);
+    
+
+    /**** PHASE 3: WRITE OUTPUT TO FILE ****/
+    FILE *output_fp = fopen(outputfile, "w");
+    if (output_fp == NULL) {
+        perror("Failed to open output file");
+        free(arr);
+        return 1;
+    }
+
+    for (int i = 0; i < N; i++) {
+        fprintf(output_fp, "%d ", arr[i]);
+    }
+    fprintf(output_fp, "\n");
+    fclose(output_fp);
+
+    free(arr);
+    return 0;
+}
+
+void* global_sort(void* targs) {
 
     // copy input
     const args_t*        args   = (args_t*) targs;
@@ -204,125 +302,26 @@ static void* global_sort(void* targs) {
     return NULL;
 }
 
+// Standard quicksort function
+void local_sort(int *arr, int begin, int end) {
+    if (begin >= end) return;
 
-int main(int ac, char** av) {
+    int pivot = arr[end]; 
+    int i = begin - 1;
 
-    // parse input
-    if (ac != 6) {
-        printf("Usage: ./%s N input output NT S\n", av[0]);
-        printf("N - Number of Elements to Sort\n");
-        printf("input - Input Numbers Dataset Filepath (generate with gen_data.c)\n");
-        printf("output - Filepath to store the sorted elements\n");
-        printf("NT - Number of Processors to Utilise\n");
-        printf("strat - Strategy to Select the pivot element:\n");
-        printf("\t\'a\' - Pick Median of Processor-0 in Processor Group\n");
-        printf("\t\'b\' - Select the mean of all medians in respective processor set\n");
-        printf("\t\'c\' - Sort the medians and select the mean value of the two middlemost medians in each processor set\n");
-
-        return 1;
-    }
-    int N = atoi(av[1]);
-    char *inputfile = av[2];
-    char *outputfile = av[3];
-    int NT = atoi(av[4]);
-    char strat = av[5][0];
-
-    /**** PHASE 1: READ INPUT FROM FILE ****/
-
-    int *arr = (int *)calloc(N, sizeof(int));
-    if (arr == NULL) {
-        perror("Memory allocation failed");
-        return 1;
-    }
-
-    FILE *input_fp = fopen(inputfile, "r");
-    if (input_fp == NULL) {
-        perror("Failed to open input file");
-        free(arr);
-        return 1;
-    }
-
-    // Reading integers from the input file
-    for (int i = 0; i < N; i++) {
-        if (fscanf(input_fp, "%d", &arr[i]) != 1) {
-            printf("Error reading number at index %d\n", i);
-            break;
+    for (int j = begin; j < end; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
         }
     }
-    fclose(input_fp);
 
+    int temp = arr[i + 1];
+    arr[i + 1] = arr[end];
+    arr[end] = temp;
 
-    /**** PHASE 2: SORT INPUT ****/
-    int** thread_local_arr = (int**) malloc(NT * sizeof(int*)); // local subarrays
-    int** exchange_arr = (int**) malloc(NT * sizeof(int*)); // remote subarrays
-    int*  exchange_arr_sizes = (int*)  malloc(NT * sizeof(int )); // number of elements 'sent'
-    int*  pivots  = (int*)  malloc(NT * sizeof(int )); // pivot element for each thread
-    int*  local_sizes  = (int*)  malloc(NT * sizeof(int )); // number of elements in each thread in arr
-    int*  medians  = (int*)  malloc(NT * sizeof(int )); // median of each subarray
-    
-    pthread_barrier_t* bar_pair = (pthread_barrier_t*) malloc(NT * sizeof(pthread_barrier_t));
-    for (int i = 0; i < NT; i++)
-        pthread_barrier_init(bar_pair + i, NULL, 2);
-    
-    // let T = 16, then
-    // bar_group = [ 0, 0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 6, 7]
-    // b_counts = [16, 8, 8, 4, 4, 4, 4, 2, 2, 2, 2, 2, 2, 2, 2]
-    // num_bs_g =  1  +  2  +     4     +          8
-    int bar_group_count = 0;
-    for (int j = 1; j < NT; j = j << 1)
-        bar_group_count += j;  
-    pthread_barrier_t* bar_group = (pthread_barrier_t*) malloc(bar_group_count * sizeof(pthread_barrier_t));
-    int l = 0;
-    for (int j = 1; j < NT; j = j << 1)
-        for (int k = 0; k < j; k++)
-            pthread_barrier_init(bar_group + l++, NULL, NT / j);
-    
-    static_args_t static_args = {N, NT, strat, thread_local_arr, exchange_arr, arr, exchange_arr_sizes, pivots, local_sizes, medians, bar_pair, bar_group};
-    pthread_t* threads = (pthread_t*) malloc(NT * sizeof(pthread_t));
-    
-    // fork
-    for (int i = 0; i < NT; i++) {
-        // malloc because otherwise it will reuse pointers or something
-        args_t* targs = (args_t*) malloc(sizeof(args_t));
-        targs->threadid = i;
-        targs->static_args = &static_args;
-        pthread_create(threads + i, NULL, global_sort, (void*) targs);
-    }
-    
-    // join
-    for (int i = 0; i < NT; i++)
-        pthread_join(threads[i], NULL);
-    
-    // free shared memory
-    free(thread_local_arr);
-    free(exchange_arr);
-    free(exchange_arr_sizes);
-    free(pivots);
-    free(local_sizes);
-    free(medians);
-    free(threads);
-    for (int i = 0; i < NT; i++)
-        pthread_barrier_destroy(bar_pair + i);
-    for (int i = 0; i < bar_group_count; i++)
-        pthread_barrier_destroy(bar_group + i);
-    free(bar_pair);
-    free(bar_group);
-    
-
-    /**** PHASE 3: WRITE OUTPUT TO FILE ****/
-    FILE *output_fp = fopen(outputfile, "w");
-    if (output_fp == NULL) {
-        perror("Failed to open output file");
-        free(arr);
-        return 1;
-    }
-
-    for (int i = 0; i < N; i++) {
-        fprintf(output_fp, "%d ", arr[i]);
-    }
-    fprintf(output_fp, "\n");
-    fclose(output_fp);
-
-    free(arr);
-    return 0;
+    local_sort(arr, begin, i);
+    local_sort(arr, i + 2, end);
 }
